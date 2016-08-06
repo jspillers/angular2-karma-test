7c98872a140d6c6ac9a9304948b612246057061d
Jrod <jarrod@jtms.net>
2016-08-06 11:59:46 -0400

    initial

---
+++ app/app.component.spec.ts
@@ -0,0 +1,8 @@
+import {it, describe, expect, beforeEach, inject} from '@angular/core/testing';
+import {AppComponent} from "./app.component";
+
+describe('AppComponent Tests', () => {
+    it('does a thing', () => {
+        expect(2 + 2).toEqual(4);
+    });
+});
---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App Its really good</h1>'
+})
+export class AppComponent { }
